92fc15d37e0721e9a7f9ab022204c265368b340c
Transfomer Wakanda <w4k4nda@kali.kalilinux>
2025-06-18 16:26:33 +0300

    successfuly added JWT email-based authentication and tested token endpoints

--- Ai_offline_backend/users/urls.py
+++ Ai_offline_backend/users/urls.py
@@ -1,7 +1,12 @@
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from django.urls import path 
 from .views import RegisterView, LoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+
+    #JWT token routes
+    path('token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh')
 ]
