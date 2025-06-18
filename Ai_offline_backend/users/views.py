946e2f880ca3315bbb6310eff20dd456fb9feb9e
Transfomer Wakanda <w4k4nda@kali.kalilinux>
2025-06-18 15:40:11 +0300

    created login serializer for token generation

--- Ai_offline_backend/users/views.py
+++ Ai_offline_backend/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from . models import CustomUser
+from . serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset=CustomUser.objects.all()
+    permission_classes=[AllowAny]
+    serializer_class=RegisterSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['role'] = user.role
+        return token
+
+class LoginView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
