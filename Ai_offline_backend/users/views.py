b0cd619f33b53a867608eb468360117d7336bc6e
Transfomer Wakanda <w4k4nda@kali.kalilinux>
2025-06-18 16:01:27 +0300

    Fix: Corrected missing .as_view() in login and register URL patterns

--- Ai_offline_backend/users/views.py
+++ Ai_offline_backend/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from . models import CustomUser
-from . serializers import RegisterSerializer
+from . serializers import RegisterSerializers
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -10,7 +10,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class RegisterView(generics.CreateAPIView):
     queryset=CustomUser.objects.all()
     permission_classes=[AllowAny]
-    serializer_class=RegisterSerializer
+    serializer_class=RegisterSerializers
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
