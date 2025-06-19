9815372e918c412cacaff23a1935c0cf419cf771
Transfomer Wakanda <w4k4nda@kali.kalilinux>
2025-06-19 11:06:35 +0300

    Created role-specific views foe student,teacher and admin

--- Ai_offline_backend/users/views.py
+++ Ai_offline_backend/users/views.py
@@ -5,6 +5,9 @@ from . serializers import RegisterSerializers
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .permissions import IsAdmin,IsStudent,IsTeacher
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
@@ -21,3 +24,20 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class LoginView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+
+class StudentView(APIView):
+    permission_classes = [IsStudent]
+    def get(self,request):
+        return Response({"message":"Welcome,student"})
+    
+class TeacherView(APIView):
+    permission_classes = [IsTeacher]
+    def get(self,reuest):
+        return Response({"message":"Welcome,admin"})
+    
+class AdminView(APIView):
+    permission_classes = [IsAdmin]
+    def get(self,request):
+        return Response({"message":"Welcome,Admin"})
+        
