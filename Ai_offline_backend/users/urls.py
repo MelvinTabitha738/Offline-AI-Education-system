be09d9c4b962f6854dc67e74edfdcb566f12de47
Transfomer Wakanda <w4k4nda@kali.kalilinux>
2025-06-19 11:10:42 +0300

    Added URL Routes for student,teacher,and admin dashboards

--- Ai_offline_backend/users/urls.py
+++ Ai_offline_backend/users/urls.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from django.urls import path 
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView,StudentView,TeacherView,AdminView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -8,5 +8,9 @@ urlpatterns = [
 
     #JWT token routes
     path('token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
-    path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh')
+    path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
+    path('student/dashboard/',StudentView.as_view(),name='student_dashboard'),
+    path('teacher/dashboard/',TeacherView.as_view(),name='teacher_dashboard'),
+    path('admin/dashboard/',AdminView.as_view(),name='admin_dashboard'),
+    
 ]
